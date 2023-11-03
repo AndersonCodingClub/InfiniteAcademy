f4cd2048f006968c1479aa816afb9ed35c7ce2e8
Zain Javaid <zainmfj@gmail.com>
2023-11-02 19:41:15 -0500

    Started homepage
    Co-authored-by: GregShatsman <GregShatsman@users.noreply.github.com>
    Co-authored-by: AKAvery <AKAvery@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Home'
+    return render_template('index.html')
+
+@app.route('/programming')
+def programming():
+    return render_template('programming.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+#http://10.9.156.176:5000
