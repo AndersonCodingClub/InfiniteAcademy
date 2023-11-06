2f6a1ce428b56915fee57517f93aaf9e3cabdd59
Zain Javaid <zainmfj@gmail.com>
2023-11-06 16:52:24 -0600

    Added routes for each subject
    Co-authored-by: GregShatsman <GregShatsman@users.noreply.github.com>

--- app.py
+++ app.py
@@ -11,7 +11,13 @@ def index():
 def programming():
     return render_template('programming.html')
 
+@app.route('/investing')
+def investing():
+    return render_template('investing.html')
+
+@app.route('/math')
+def math():
+    return render_template('math.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
-#http://10.9.156.176:5000
